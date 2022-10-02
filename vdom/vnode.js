aa79826cd73733a7fe32d9832ccd40162e658841
gygirl <1308265464@qq.com>
2022-10-02 23:31:54 +0800

    '提交作业'

--- vdom/vnode.js
+++ vdom/vnode.js
@@ -1,6 +1,5 @@
 /*
-简单实现incremental-dom中的关键api, 需要考虑同时存在多个vdom的情况。
-不考虑element的情况，直接转换为jsonDom
+简单实现incremental-dom中的关键api, 需要考虑同时存在多个vdom的情况。不考虑element的情况，直接转换为jsonDom
 返回结果示例
 {"tagName":"div","children":[{"tagName":"p","text":"1"}],"text":"2"}
 */
